8d9accfe30ecf0dd2f82abb564765852b7319cfd
austinhofmann <a916h636@ku.edu>
2015-03-02 18:40:56 -0600

    Added execute()
    Added execute(), error messages might be wrong, but they throw errors
    correctly. Need to implement I/O redirection and do clean-up afterwards

--- main.c
+++ main.c
@@ -5,8 +5,57 @@
 #include <sys/types.h>
 
 
-//TODO: Implement execute
-void execute() {
+//TODO: Implement I/O redirection & clean-up 
+void execute(char * executeInput) {
+
+	char * currentInput = strdup(executeInput);
+	char * tokenizedInput = strtok(executeInput, " \n");
+	
+	//find a better way to do this... removes newlines
+	char * ptrA; 
+	char * ptrB;
+  	for (ptrA=ptrB=currentInput;*ptrB=*ptrA;ptrB+=(*ptrA++!='\n'));
+	//END
+
+	int stringIndex = strlen(tokenizedInput);
+	
+	//PROTOTYPE:: char *strncpy(char *dest, const char *src, size_t n)
+	strncpy(currentInput, &currentInput[stringIndex + 1], strlen(currentInput));
+	
+	
+	
+	//start process
+	pid_t pidProcess;
+	pidProcess = fork();
+	
+	if(pidProcess == 0) {
+	
+		//TODO: I/O redirection (to-file and from-file)
+		
+		
+		if(strlen(currentInput) > 0 ) {
+			if(execlp(tokenizedInput, tokenizedInput, currentInput, (char *)NULL) < 0) { //see if this works, if not you can edit out the (char *)
+				fprintf(stderr, "\nError: incorrect input (input remaining)\n"); //might need to correct these error messages, i think this is what they handle
+				exit(0);
+			}
+		}else{
+			if(execlp(tokenizedInput, tokenizedInput, (char *)NULL) < 0) {
+				fprintf(stderr, "\nError: incorrect input (no input remaining)\n");
+				exit(0);
+			}
+		}
+
+
+	}else{
+	
+		int waitPidStatusPtr;
+		waitpid(pidProcess, &waitPidStatusPtr, 0);
+		
+		if(waitPidStatusPtr == 1) {
+			fprintf(stderr, "\n%s\n", "Error: waitpid in execute()");
+		}
+		
+	}
 }
 
 //TODO: Implement cd
